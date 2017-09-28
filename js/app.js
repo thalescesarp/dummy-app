2d198fcfab37d3d5bb3fc70ea8aa8eaf51db2a70
thalescesarp <thalescesarp@gmail.com>
2017-09-28 08:09:30 -0300

    syntax errors comment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//I like using self invoking functions with 'use strict'.
+//With it I avoid poluting the global scope and get better syntax errors.
+//Having syntax errors is great because they prevent some mistyping and 
+//bad naming bugs which can be hard to debug because sometimes we just 
+//can't see them in our own code. Countless hours have been saved! ;)
+//I also use jshint as a linting tool.
+
 (function () {
     'use strict';
     angular.module('dummyApp', [
