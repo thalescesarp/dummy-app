45cbf8de4f5ae791bd566c1df3bd626c6013a81e
thalescesarp <thalescesarp@gmail.com>
2017-09-29 00:24:01 -0300

    grid test

--- views/home/home_controller.js
+++ views/home/home_controller.js
@@ -5,4 +5,28 @@ angular.module("dummyApp")
 
     $scope.content = "Scope Test";
 
+    var Column = DummyApp.constructors.ColumnOptions;
+    
+    function Rider(email, city, rideInGroup, daysOfWeek, registrationDay) {
+        this.email = email;
+        this.city = city;
+        this.rideInGroup = rideInGroup;
+        this.daysOfWeek = daysOfWeek;
+        this.registrationDay;
+    }
+
+    $scope.bikersGridOptions = new DummyApp.constructors.GridOptions(
+        [
+            new Column("E-mail", function(row){ return row.email; }),
+            new Column("City", function(row){ return row.city; }),
+            new Column("Ride in group", function(row){ return row.rideInGroup; }),
+            new Column("Days of the week", function(row){ return row.daysOfWeek; }),
+            new Column("Registration day", function(row){ return row.registrationDay; })
+        ],
+        [
+            new Rider("James Isaac Neutron", "neutron@atom.com", true, "Mon", new Date()),
+            new Rider("Carl Wheezer", "carl@biker.com", true, "Tue", new Date()),
+            new Rider("Thomas Johnson", "tjhonson@bikers.com", true, "Mon Fri", new Date()),
+        ]
+    )
 }]);
